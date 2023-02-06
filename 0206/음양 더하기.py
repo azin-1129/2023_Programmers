bc737e35a3c9318d5fb1ea266549054dde271972
azin-1129 <nuestiloveyou@gmail.com>
2023-02-06 18:59:23 +0900

    1859

---
+++ 0206/음양 더하기.py
@@ -0,0 +1,10 @@
+def solution(absolutes, signs):
+    sum=0
+
+    for i in range(len(absolutes)):
+        if signs[i]:
+            sum+=absolutes[i]
+        else:
+            sum-=absolutes[i]
+
+    return sum
